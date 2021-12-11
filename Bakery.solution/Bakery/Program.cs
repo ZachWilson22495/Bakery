afab1bbeb78a068559fd508802e72d583c8579eb
Zach Wilson <zwilson22495@gmail.com>
2021-12-10 16:57:27 -0800

    Pushed to Github

--- Bakery.solution/Bakery/Program.cs
+++ Bakery.solution/Bakery/Program.cs
@@ -13,17 +13,31 @@ namespace UI
       {
         Console.WriteLine("How many loaves would you like? They're $5 each and we're currently doing a 'buy 2, get 1 free' deal. For supply reasons, I can only sell up to 12 loaves per customer.");
         string number = (Console.ReadLine()).ToLower();
-        Bread breadPurchase = new Bread(number);
-        int breadTotal = breadPurchase.GetBreadPrice(number);
-        Console.WriteLine("Lovely. That'll be $" + breadTotal.ToString() + " total. Have a great day! Tell your friends!");
+        if (number == "1", || number == "one", || number == "2", || number == "two", || number == "3", || number == "three", || number == "4", || number == "four", || number == "5", || number == "five", || number == "6", || number == "six", || number == "7", || number == "seven", || number == "8", || number == "eight", || number == "9", || number == "nine", || number == "10", || number == "ten", || number == "11", || number == "eleven", || number == "12", || number == "twelve")
+        {
+          Bread breadPurchase = new Bread(number);
+          int breadTotal = breadPurchase.GetBreadPrice(number);
+          Console.WriteLine("Lovely. That'll be $" + breadTotal.ToString() + " total. Have a great day! Tell your friends!");
+        }
+        else
+        {
+          Console.WriteLine("Can't sell ya that much. Sorry boss.");
+        }
       }
       else if ((answer == "pastries") || (answer == "pastry"))
       {
         Console.WriteLine("How many pastries are we getting today? They're each 2$, but if you buy in bulk, I'll reduce the total cost by $1. If you buy six, I'll make it an even $10.");
         string number = (Console.ReadLine()).ToLower();
-        Pastry pastryPurchase = new Pastry(number);
-        int pastryTotal = pastryPurchase.GetPastryPrice(number);
-        Console.WriteLine(" That'll be $" + pastryTotal.ToString() + " total! Have yourself a wonderful day, and try to enjoy those pastries as much as I enjoyed making them!");
+        if (number == "1", || number == "one", || number == "2", || number == "two", || number == "3", || number == "three", || number == "4", || number == "four", || number == "5", || number == "five", || number == "6", || number == "six")
+        {
+          Pastry pastryPurchase = new Pastry(number);
+          int pastryTotal = pastryPurchase.GetPastryPrice(number);
+          Console.WriteLine(" That'll be $" + pastryTotal.ToString() + " total! Have yourself a wonderful day, and try to enjoy those pastries as much as I enjoyed making them!");
+        }
+        else
+        {
+          Console.WriteLine("Can't sell ya that much. Sorry boss.");
+        }
       }
       else if (answer == "both")
       {
@@ -38,6 +52,10 @@ namespace UI
         int finalTotal = breadTotal + pastryTotal;
         Console.WriteLine("Alrighty. $" + breadTotal.ToString() + " for the bread, plus $" + pastryTotal.ToString() + " for the pastries brings your total to $" + finalTotal.ToString() + ". Thanks for visiting Pierre's today! Hope to see you again soon.");
       }
+      else 
+      {
+        Console.WriteLine("What was that? I don't think I sell those. Can you say it again? Pierre's ears aren't what they used to be.");
       }
     }
   }
+}
